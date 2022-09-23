9b6f183c4e5cda1733d7150a9ae660b88b6ef587
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2022-09-23 23:17:23 +0100

    All tasks completed

--- lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
+++ lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'udacity-learn.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'images'
 
 
 class AuthError(Exception):
@@ -104,21 +104,43 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
+        
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorised',
+            'description': 'Permissions not found.'
+        }, 403)
+        
+    return True
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
 
-    return wrapper
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            jwt = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(jwt)
+            except:
+                abort(401)
+                
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
 
 @app.route('/headers')
-@requires_auth
+@requires_auth('get:images')
 def headers(payload):
+    # auth_header = request.headers.get('Authorization', None)
+    # token = auth_header.split()[1]
     print(payload)
     return 'Access Granted'
